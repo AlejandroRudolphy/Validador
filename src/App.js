dad545d4dc05be6bdb031388c58f27fbeca97305
Alejandro <alejandro.rudolphy@alumnos.uv.cl>
2020-11-30 02:11:25 -0300

    validador

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom";
 
 import {Button} from "@material-ui/core"
 import MiComponente from './MiComponente';
@@ -8,10 +9,15 @@ import MiValidador from './MiValidador';
 
 function App() {
   return (
-    <div className="App">n
+    <div className="App">
       <header className="App-header">
-        <p>Clave uvalpo</p>
-        <MiValidador  valor = "123344" />
+      <img 
+      src="https://i.pinimg.com/originals/32/82/36/328236d1e2cecebea49a5a0ceda62d95.jpg"
+      widht="400"
+      height="400"
+      />
+        <p>Clave U de Valpo</p>
+        <MiValidador  valor = "123456789" />
       </header>
     </div>
   );
--- src/MiValidador.js
+++ src/MiValidador.js
@@ -15,7 +15,10 @@ export default function MiValidador(params) {
         setClave(e.target.value);
     }
 
-     if(clave === "uvalpo") return <p>Maravilloso</p>
+     if(clave === "Bolano12") 
+     return <img src="https://image.shutterstock.com/image-vector/emoji-emoticon-pulling-his-finger-600w-1607332054.jpg"
+            width="100"
+            height="100"/>
 
      return  (<div>
         <input type="text" value={rutIngresado} onChange={change}  />
